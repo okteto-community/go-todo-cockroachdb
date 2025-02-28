73578a6694c714c4a232b5132e2c2b6eac9aee23
Ramiro <ramiro@okteto.com>
2025-02-27 17:14:21 -0800

    initial version

---
+++ README.md
@@ -0,0 +1,10 @@
+1. create cockroachdb cloud cluster
+1. create admin variables with the values obtained from cockroachdb:
+
+            COCKROACH_CLUSTERID
+            COCKROACH_USERNAME
+            COCKROACH_PASSWORD
+            COCKROACH_HOST
+            COCKROACH_PORT
+            COCKROACH_DATABASE
+1. Deploy your development environment by running `okteto deploy`
---
+++ todo/main.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"os"
+	"sort"
+
+	"github.com/google/uuid"
+	"github.com/gorilla/mux"
+
+	log "github.com/sirupsen/logrus"
+
+	"github.com/rs/cors"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+// Todo todo struct
+type Todo struct {
+	gorm.Model `json:"-"`
+	Task       string `json:"task"`
+	ID         string `json:"id"`
+}
+
+func healthz(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+}
+
+func createItem(w http.ResponseWriter, r *http.Request) {
+	task := r.FormValue("task")
+	todo := &Todo{Task: task, ID: uuid.New().String()}
+	result := db.Create(todo)
+	if result.Error != nil {
+		log.Error(result.Error)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(todo)
+	log.Info("saved todo item")
+}
+
+func deleteItem(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	if id == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	todo := &Todo{ID: id}
+	result := db.Delete(todo)
+	if result.Error != nil {
+		log.Error(result.Error)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	log.Info("deleted todo item")
+}
+
+func getItems(w http.ResponseWriter, r *http.Request) {
+	var all []Todo
+	result := db.Find(&all)
+	if result.Error != nil {
+		log.Error(result.Error)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	sort.Slice(all, func(i, j int) bool {
+		return all[i].Task > all[j].Task
+	})
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(all)
+	log.Infof("got %d items", len(all))
+}
+
+func main() {
+	dsn := os.Getenv("DSN")
+	var err error
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	db.AutoMigrate(&Todo{})
+
+	log.Infof("Connected to DB")
+
+	router := mux.NewRouter()
+	router.HandleFunc("/healthz", healthz).Methods("GET")
+	router.HandleFunc("/todo", getItems).Methods("GET")
+	router.HandleFunc("/todo", createItem).Methods("POST")
+	router.HandleFunc("/todo/{id}", deleteItem).Methods("DELETE")
+	router.PathPrefix("/").Handler(http.FileServer(http.Dir("./static/")))
+
+	handler := cors.New(cors.Options{
+		AllowedMethods: []string{"GET", "POST", "DELETE", "PATCH", "OPTIONS"},
+	}).Handler(router)
+
+	log.Info("Starting API server...")
+	http.ListenAndServe(":8080", handler)
+}
